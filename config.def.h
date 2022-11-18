bb05232a10a37753e976590b8c49f4727dd7d863
royik <ryoheiikezawa@protonmail.com>
2022-11-18 11:31:53 +0900

    update

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 
 static int fuzzy = 1;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 
-static int min_width = 600;                    /* minimum width when centered */
+static int min_width = 1000;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=16"
@@ -14,15 +14,15 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    [SchemeNorm] = { "#ebdbb2", "#282828" },
+	[SchemeSel] = { "#ebdbb2", "#98971a" },
+	[SchemeSelHighlight] = { "#ffee00", "#98971a" },
+	[SchemeNormHighlight] = { "#ffc978", "#282828" },
+	[SchemeOut] = { "#ebdbb2", "#8ec07c" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 20;
-static unsigned int columns    = 1;
+static unsigned int lines      = 12;
+static unsigned int columns    = 2;
 
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 0;
@@ -35,4 +35,4 @@ static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 3;
