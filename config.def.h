05844682e6860c24d01ce3f4daba698c9663d40f
royik <ryoheiikezawa@protonmail.com>
2022-11-18 11:08:43 +0900

    lineheight

--- config.def.h
+++ config.def.h
@@ -24,6 +24,10 @@ static const char *colors[SchemeLast][2] = {
 static unsigned int lines      = 20;
 static unsigned int columns    = 1;
 
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 0;
+static unsigned int min_lineheight = 8;
+
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
