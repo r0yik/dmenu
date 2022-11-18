23b85bb2fc5b387e3237706d94242b946e9a6345
royik <ryoheiikezawa@protonmail.com>
2022-11-18 10:12:44 +0900

    fuzzymatch

--- config.def.h
+++ config.def.h
@@ -3,6 +3,9 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
+
+static int fuzzy = 1;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+
 static int min_width = 600;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
