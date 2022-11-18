f2882e4df5f61875a6e8fdd31c856c4bbafc5a6a
royik <ryoheiikezawa@protonmail.com>
2022-11-18 10:00:17 +0900

    update

--- config.def.h
+++ config.def.h
@@ -3,10 +3,10 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int min_width = 600;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=16"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -17,7 +17,7 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 20;
-static unsigned int columns    = 4;
+static unsigned int columns    = 1;
 
 /*
  * Characters not considered part of a word while deleting words
